0bcb724a475b75ddceee4b6c73a5ef06379cf79c
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-05-02 04:08:57 +0600

    landing page setup

---
+++ src/pages/shared/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Footer = () => {
+    return (
+        <Container>
+            <small className='text-center'>Thios is footer</small>
+        </Container>
+    );
+};
+
+export default Footer;
