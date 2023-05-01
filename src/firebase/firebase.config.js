982ac0a7227fa98c09d31bfcaeb10e4fa4403229
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-05-02 03:28:08 +0600

    included firebase config file and install react bootstrap

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDEQsopHmxKeibB7W8fw8aj7v6i1otLT2Y",
+  authDomain: "master-chef-ecb66.firebaseapp.com",
+  projectId: "master-chef-ecb66",
+  storageBucket: "master-chef-ecb66.appspot.com",
+  messagingSenderId: "879518755871",
+  appId: "1:879518755871:web:448334b503a3196d71392a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
